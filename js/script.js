53ed10b173da665ce7932fcc2ccf819ed50167b0
francesca-giorno <francescagiorno1996@gmail.com>
2020-10-19 16:23:36 +0200

    add password

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var nomeUtente = prompt('Qual è il tuo nome?');
 
+
 var cognomeUtente = prompt('Qual è il tuo cognome?');
 
-var coloreUtente = pompt('Qual è il tuo cognome preferito?');
+var coloreUtente = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById("password").innerHTML= nomeUtente + cognomeUtente + coloreUtente + '19';
